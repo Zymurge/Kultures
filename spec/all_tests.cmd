3e32eff78008413ea12739359edd71c8bb66fc80
Mark Davis <zymurge@gmail.com>
2017-05-03 21:20:32 -0400

    Changed test file naming to isolate pure unit from those with dependencies

--- spec/all_tests.cmd
+++ spec/all_tests.cmd
@@ -1,3 +1,3 @@
-jasmine spec\kulture_spec.js
-jasmine-node spec\utils_spec.js
-jasmine-node spec\rest_spec.js
+call node_modules/.bin/mocha spec\kulture_spec.js
+call node_modules/.bin/mocha spec\test*
+call node_modules/.bin/mocha spec\_test*
